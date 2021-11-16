62607d26e034853dee1adcc8a79f0b2635c8cee6
renatavosten <57946609+renatavosten@users.noreply.github.com>
2021-11-16 19:40:54 +0100

    Problem sa fontom-fixan

--- app/src/main/java/android/unipu/kvizalica/MainActivity.java
+++ app/src/main/java/android/unipu/kvizalica/MainActivity.java
@@ -2,13 +2,34 @@ package android.unipu.kvizalica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // varijabla za gumb Igraj
+    Button igraj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // za gumb Igraj
+        igraj = findViewById(R.id.button_igraj);
+
+        // dodavanje listenera za gumb Igraj
+        igraj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pozivanje aktivnosti ChooseCategory
+                Intent intent = new Intent(MainActivity.this, ChooseCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
