2b02264286c1c68f8183cbea433255121e9c796a
renatavosten <57946609+renatavosten@users.noreply.github.com>
2021-11-28 21:06:00 +0100

    Layout do voce_pitanja_level1 gotovo

--- app/src/main/java/android/unipu/kvizalica/VoceLeveliActivity.java
+++ app/src/main/java/android/unipu/kvizalica/VoceLeveliActivity.java
@@ -6,14 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 public class VoceLeveliActivity extends AppCompatActivity {
-
-
-
-
-    Button Level1;
+    Button buttonLevel1, buttonLevel2, buttonLevel3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +16,21 @@ public class VoceLeveliActivity extends AppCompatActivity {
         setContentView(R.layout.activity_voce_leveli);
 
 
-        // za gumb Voce
-        Level1 = findViewById(R.id.voce_level1);
+        // za gumb Level 1
+        buttonLevel1 = findViewById(R.id.voce_level1);
 
-        // dodavanje listenera za gumb Igraj
-        Level1.setOnClickListener(new View.OnClickListener() {
+        // za gumb Level 2
+        buttonLevel2 = findViewById(R.id.voce_level2);
+        buttonLevel2.setEnabled(false);
+
+        // za gumb Level 3
+        buttonLevel3 = findViewById(R.id.voce_level3);
+
+        // dodavanje listenera za gumb Level 1
+        buttonLevel1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // pozivanje aktivnosti ChooseCategory
+                // pozivanje aktivnosti VocePitanjaLevel1Activity
                 Intent intent = new Intent(VoceLeveliActivity.this, VocePitanjaLevel1Activity.class);
                 startActivity(intent);
             }
--- app/src/main/java/android/unipu/kvizalica/VocePitanjaLevel1Activity.java
+++ app/src/main/java/android/unipu/kvizalica/VocePitanjaLevel1Activity.java
@@ -13,30 +13,31 @@ import android.widget.ProgressBar;
 
 public class VocePitanjaLevel1Activity extends AppCompatActivity {
     CountDownTimer countDownTimer;
-    int timerValue=20;
+    int timerValue = 20;
     ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voce_pitanja_level1);
-        progressBar=findViewById(R.id.progress_bar);
-        countDownTimer=new CountDownTimer(20000,1000) {
+
+        progressBar = findViewById(R.id.progress_bar);
+        countDownTimer = new CountDownTimer(20000,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                timerValue=timerValue-1;
+                timerValue = timerValue-1;
                 progressBar.setProgress(timerValue);
             }
 
             @Override
             public void onFinish() {
-                Log.i("1","Zavrsio sam");
-                Dialog dialog=new Dialog(VocePitanjaLevel1Activity.this);
+                //Log.i("1","Zavrsio sam");
+                Dialog dialog = new Dialog(VocePitanjaLevel1Activity.this);
                 dialog.setContentView(R.layout.time_out);
-                Log.i("2","Trebam se pokazati");
+                //Log.i("2","Trebam se pokazati");
 
                 dialog.show();
-                Log.i("Pokazao sam se","Pokazao sam se");
+                //Log.i("Pokazao sam se","Pokazao sam se");
             }
         }.start();
     }
