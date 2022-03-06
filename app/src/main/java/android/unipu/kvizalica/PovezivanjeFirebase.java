60c4c01b2b5b267feb74616f33faa32a4970e396
Lorena Jeger <lorena.jeger1007@gmail.com>
2022-03-06 13:24:36 +0100

    novaAktivnost

---
+++ app/src/main/java/android/unipu/kvizalica/PovezivanjeFirebase.java
@@ -0,0 +1,16 @@
+package android.unipu.kvizalica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PovezivanjeFirebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_povezivanje_firebase);
+
+
+    }
+}
